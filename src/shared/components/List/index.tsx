cd0244bea6adedbbc4e49026a36e33f32b50eb85
kimhuy011199 <kimhuy011199@gmail.com>
2022-06-17 12:32:39 +0700

    Implement list component

---
+++ src/shared/components/List/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import style from './style.module.css';
+
+interface ListInterface {
+  data: any;
+  heading?: string;
+  emptyListContent?: string;
+  children?: any;
+}
+
+const List = (props: ListInterface) => {
+  const { data, heading, emptyListContent, children } = props;
+
+  return (
+    <div className={style.listContainer}>
+      <h3 className={style.heading}>{heading}</h3>
+      {data.length > 0 ? (
+        <ul className={style.list}>
+          {data.map((item: any, index: number) => (
+            <li key={item?._id + index} className={style.item}>
+              {React.cloneElement(children, { item })}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h4 className={style.emptyListContent}>{emptyListContent}</h4>
+      )}
+    </div>
+  );
+};
+
+export default List;
