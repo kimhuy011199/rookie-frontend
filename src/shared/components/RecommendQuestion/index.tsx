d42650cfa82d2c8e998f714011b003b8c54c2f4f
kimhuy011199 <kimhuy011199@gmail.com>
2022-06-17 12:36:04 +0700

    Update list to recommendation section

--- src/shared/components/RecommendQuestion/index.tsx
+++ src/shared/components/RecommendQuestion/index.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { getRecommendQuestions } from '../../../stores/questions/questionSlice';
-import { Question } from '../../constants/types/Question';
-import style from './style.module.css';
+import List from '../List';
+import QuestionLinkItem from '../QuestionLinkItem';
 
 interface RecommendQuestionInterface {
   questionId: string;
@@ -14,7 +13,6 @@ const RecommendQuestion = (props: RecommendQuestionInterface) => {
   const { questionId } = props;
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const generateLink = (id: string) => `/questions/${id}`;
 
   const { recommend } = useSelector((state: any) => state.questions);
 
@@ -25,27 +23,13 @@ const RecommendQuestion = (props: RecommendQuestionInterface) => {
   }, [questionId, dispatch]);
 
   return (
-    <div className={style.container}>
-      <h3 className={style.heading}>{t('questions.recommendation')}</h3>
-      {recommend.length > 0 ? (
-        <ul className={style.list}>
-          {recommend.map((item: Question) => (
-            <li key={item._id} className={style.item}>
-              <div className={style.question}>
-                <Link className={style.link} to={generateLink(item._id)}>
-                  <h3 className={style.title}>{item.title}</h3>
-                  <p className={style.content}>{item.content}</p>
-                </Link>
-              </div>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <h4 className={style.noRecommend}>
-          {t('questions.no_recommendation')}
-        </h4>
-      )}
-    </div>
+    <List
+      data={recommend}
+      heading={t('questions.recommendation')}
+      emptyListContent={t('questions.no_questions')}
+    >
+      <QuestionLinkItem />
+    </List>
   );
 };
 
