121a7d3a6db60eebcb808158b7d61de59ba373c4
kimhuy011199 <kimhuy011199@gmail.com>
2022-07-08 06:23:05 +0700

    Update code text color

--- src/shared/components/Markdown/index.tsx
+++ src/shared/components/Markdown/index.tsx
@@ -9,7 +9,7 @@ const MarkdownRender = (props: MarkdownRenderInterface) => {
   const { content } = props;
 
   return (
-    <div className="max-w-none prose prose-p:text-white prose-h1:text-white prose-h2:text-white prose-h3:text-white prose-h4:text-white dark:prose-invert prose-h1:text-2xl prose-h1:my-4 prose-h2:text-xl prose-h2:my-3 prose-h3:text-lg prose-h3:my-3.5 prose-h4:text-md prose-h4:my-3.5 prose-a:text-primary prose-a:no-underline prose-blockquote:my-3 prose-li:my-2 prose-img:my-4 prose-p:my-3.5 prose-pre:my-4 prose-hr:my-5 prose-pre:bg-dark-3 prose-pre:text-white prose-p:whitespace-pre-wrap">
+    <div className="max-w-none prose prose-p:text-white prose-h1:text-white prose-h2:text-white prose-h3:text-white prose-h4:text-white dark:prose-invert prose-h1:text-2xl prose-h1:my-4 prose-h2:text-xl prose-h2:my-3 prose-h3:text-lg prose-h3:my-3.5 prose-h4:text-md prose-h4:my-3.5 prose-a:text-primary prose-a:no-underline prose-blockquote:my-3 prose-li:my-2 prose-img:my-4 prose-p:my-3.5 prose-pre:my-4 prose-hr:my-5 prose-pre:bg-dark-3 prose-pre:text-white prose-code:text-white prose-p:whitespace-pre-wrap">
       <ReactMarkdown>{content}</ReactMarkdown>
     </div>
   );
